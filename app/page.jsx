2e6ded7747ba3177c968bfffffd30f192e444a4e
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-10-14 11:00:00 +0200

    refactor: Replace Home component with new HeroCarousel system

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import HeroCarousel from './components/HeroCarousel';
+
+export default function HomePage() {
+  return (
+    <section className='flex-1'>
+      <HeroCarousel />
+    </section>
+  );
+}
