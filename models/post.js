5a3e01970029bbdd3d1b653ee361da2efc889650
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-10-16 14:29:58 +0200

    feat: Add description field to post schema fix: Correct model name casing in post export

--- models/post.js
+++ models/post.js
@@ -2,17 +2,18 @@ import { Schema, models, model } from 'mongoose';
 
 const postSchema = new Schema(
   {
-    badge: { type: String, required: true },
+    category: { type: String, required: true },
     imageUrl: { type: String, required: true },
     trending: { type: Boolean, required: true },
     author: { type: String, required: true, trim: true },
     title: { type: String, required: true, trim: true },
     comments: { type: Number, required: true, trim: true },
+    description: { type: String, required: true},
     createdAt: { type: Date, default: Date.now().toString(), required: true },
   },
   { timestamps: true }
 );
 
-const Post = models.post || model('post', postSchema);
+const Post = models.Post || model('Post', postSchema);
 
 export default Post;
