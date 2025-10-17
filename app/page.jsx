b545ff7eceac77a01bb21d4d302fb7a1b34a5bab
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-10-17 11:17:47 +0200

    feat: Add dynamic rendering configuration to HomePage refactor: Remove unused console.log in HomePage

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
+export const dynamic = 'force-dynamic'
+
 import HeroCarousel from './components/HeroCarousel';
 import Seaction from './components/Seaction';
 import { fetchPosts, fetFeaturedPosts } from '@/lib/helper';
 
 export default async function HomePage(req, res) {
-  const posts = await fetchPosts();
   const featuredPosts = await fetFeaturedPosts()
-  console.log(featuredPosts)
   return (
     <>
       <section className='flex-1'>
