d8bacef62a8c9a70fd92d0498faecd70a9d6d8a0
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-10-21 12:59:13 +0200

    feat: Add Featured Videos section to homepage refactor: Simplify grid classes in homepage sections style: Fix indentation and spacing in JSX elements

--- app/page.jsx
+++ app/page.jsx
@@ -15,6 +15,10 @@ export default async function HomePage() {
   const topStories = await fetchTopStories();
   const _2topStories = topStories.slice(0, 2);
   const _3topStories = topStories.slice(-3);
+  const _1post = topStories.slice(-2);
+  const __post = topStories.slice(-1);
+
+  const [_post] = __post;
 
   return (
     <>
@@ -32,16 +36,37 @@ export default async function HomePage() {
         title={'Top Stories'}
         link={'See All Posts'}
       >
-        <div className='grid place-items-center gap-6 md:grid-cols-2 lg:grid-cols-3'>
+        <div className='grid gap-6 lg:grid-cols-3'>
           {_2topStories.map((post) => (
             <StoryCard key={post._id} post={post} />
           ))}
 
           <div className='flex gap-2 flex-col justify-between md:h-[390px]'>
             {_3topStories.map((post) => (
-              <Story2Card key={post._id} post={post} />
+              <Story2Card key={post._id} post={post}></Story2Card>
             ))}
           </div>
+        </div>
+      </Section>
+
+      {/* featured videos */}
+      <Section
+        className='bg-dark-blue text-white'
+        title={'Featured Videos'}
+        link={'See All Videos'}
+      >
+        <div className='flex flex-col lg:flex-row lg:h-[816px] gap-4'>
+          <div className='flex flex-col w-full lg:w-1/2 gap-4'>
+            {_1post.map((post) => {
+              return <StoryCard post={post} key={post._id} />;
+            })}
+          </div>
+
+          <StoryCard
+            className='w-full lg:w-1/2 !h-[390px] lg:!h-full'
+            key={_post._id}
+            post={_post}
+          />
         </div>
       </Section>
 
